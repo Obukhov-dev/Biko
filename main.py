74b3f3b94d0eb7ba237f331304675a2138a500b8
Ivan Obuhov <65172086+Obuhov@users.noreply.github.com>
2023-03-26 18:03:56 +0500

    Beginning. import fr

---
+++ main.py
@@ -0,0 +1,26 @@
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import run_async, run_js
+
+# Для ..
+import asyncio
+
+#
+chat_msgs = []
+online_users = set()
+
+MAX_MESSAGES_COUNT = 100
+
+
+async def main():
+    """
+
+    :return:
+    """
+    pass
+
+# запускаем web сервер (стандартно tornado. Можно django flask fast api)
+if __name__ == "__main__":
+    start_server(main, debug=True, port=8080, cdn=False)
+
