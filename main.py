efb295307a49c7558ad29f408093138afb89c5a1
Ivan Obuhov <65172086+Obuhov@users.noreply.github.com>
2023-03-26 19:01:53 +0500

    Login chat user

--- main.py
+++ main.py
@@ -15,9 +15,20 @@ MAX_MESSAGES_COUNT = 100
 
 async def main():
     """
+    Главная функция при открытии приложения.
 
     :return:
     """
+    #Все glob сообщения
+    global chat_msgs
+
+    put_markdown("## Привет! Это web мессенджер Biko. \n Укажите свой ник и общайтесь свободно.")
+    msg_box = output()
+    put_scrollable(msg_box, height=300, keep_bottom=True)
+
+    nickname = await input("Войти в чат", required=True, placeholder="Ваш ник Под ним вас будут видеть другие пользователи 🤫 🤩 😄",validate=lambda n: 'Такой ник уже используется' if n in online_users or n == '!!!' else None)
+    online_users.append(('!!!!', f"{nickname} - присоединился к чату"))
+
     pass
 
 # запускаем web сервер (стандартно tornado. Можно django flask fast api)
